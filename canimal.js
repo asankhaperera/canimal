8e4fb605bd37492795960c6c14a6928d7acd3470
asankhaperera <asankha.perera@gmail.com>
2020-04-30 16:47:45 +0530

    init

---
+++ canimal.js
@@ -0,0 +1,69 @@
+const LaunchRequest_Handler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'LaunchRequest';
+    },
+    handle(handlerInput) {
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let say = 'Welcome to my Chinese Animal Game';
+        return responseBuilder
+            .speak(say)
+            .reprompt('try again, ' + say)
+            .getResponse();
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const request = handlerInput.requestEnvelope.request;
+        console.log(`Error handled: ${error.message}`);
+        // console.log(`Original Request was: ${JSON.stringify(request, null, 2)}`);
+
+        return handlerInput.responseBuilder
+            .speak(`Sorry, your skill got this error.  ${error.message} `)
+            .reprompt(`Sorry, your skill got this error.  ${error.message} `)
+            .getResponse();
+    }
+};
+
+const ChineseAnimalIntent_Handler = {
+    canHandle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' && request.intent.name === 'ChineseAnimalIntent';
+    },
+    async handle(handlerInput) {
+        const request = handlerInput.requestEnvelope.request;
+        const responseBuilder = handlerInput.responseBuilder;
+
+        let say = "hello";
+
+        return responseBuilder
+            .speak(say)
+            .reprompt('try again, ' + say)
+            .getResponse();
+    },
+};
+
+let skill;
+
+exports.handler = async (event) => {
+    
+    console.log(`REQUEST++++${JSON.stringify(event)}`);
+    if (!skill) {
+        skill = Alexa.SkillBuilders.custom().addRequestHandlers(
+            ChineseAnimalIntent_Handler,
+            LaunchRequest_Handler,
+        )
+        .addErrorHandlers(ErrorHandler)
+        .create();
+    }
+
+    const response = await skill.invoke(event, context);
+    console.log(`RESPONSE++++${JSON.stringify(response)}`);
+
+    return response;
+};
